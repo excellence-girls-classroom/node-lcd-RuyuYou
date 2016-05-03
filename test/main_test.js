476710044fab55748eb6359dc812f76436d65ae5
Ruyu You <727809166@qq.com>
2016-05-03 21:32:03 +0800

    UnitTest of stringSplit

--- test/main_test.js
+++ test/main_test.js
@@ -1,3 +1,22 @@
 /**
  * Created by you on 16-5-3.
  */
+describe('Unit Test' , function () {
+    var inputs;
+    beforeEach(function () {
+        inputs = "910";
+        var allNumbers = require('./fixtures.js').loadAllNumbers();
+    });
+
+    describe('stringSplit' , function () {
+       it('should print correct string',function () {
+           var newInputs = require ('../main/stringSplit.js').stringSplit(inputs);
+           var expectNewInputs = [
+               '9',
+               '1',
+               '0'
+           ];
+           expect(newInputs).toEqual(expectNewInputs);
+       }) ;
+    });
+});
