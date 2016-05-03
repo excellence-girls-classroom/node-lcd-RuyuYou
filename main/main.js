3b7f475a11d49bd8fce9784b3f1061805a6c5a3e
Ruyu You <727809166@qq.com>
2016-05-03 22:19:02 +0800

    write main

--- main/main.js
+++ main/main.js
@@ -1,3 +1,16 @@
 /**
  * Created by you on 16-5-3.
  */
+function printLcdDisplay(inputs) {
+    var allNumbers =require('../test/fixtures.js').loadAllNumbers();
+    
+    var newInputs = require('./stringSplit.js').stringSplit(inputs);
+    
+    var lcdDisplay = require('./buildDisplay.js').buildDisplay(newInputs,allNumbers);
+    
+    var display = require('./buildprintDisplay.js').buildprintDisplay(lcdDisplay);
+
+    console.log(display);
+}
+
+exports.printLcdDisplay = printLcdDisplay;
